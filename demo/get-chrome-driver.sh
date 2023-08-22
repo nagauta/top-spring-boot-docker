c634a1b0f27e21e7b04eaaa72c4e260032b30898
mie035 <hello@michitoshiyano.com>
2023-08-23 08:25:21 +0900

    add: shell to get chromedriver

---
+++ demo/get-chrome-driver.sh
@@ -0,0 +1,3 @@
+curl -O https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/mac-arm64/chromedriver-mac-arm64.zip
+unzip ./chromedriver-mac-arm64.zip
+mv chromedriver-mac-arm64/chromedriver ./driver
